530dc7233a0e69fc760c6495dbf1d07fdefbe5bc
UnicornDad8 <shiningforce972@gmail.com>
2025-05-24 15:33:27 -0300

    Empezar el userAuthForm para sign-in y sign-up

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home.page";
 import Error from "./pages/404.page";
 import Navbar from "./components/navbar.component";
+import UserAuthForm from "./pages/userAuthForm.page";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navbar />}>
-          <Route path="sign-in" element={<h1>Sign In Page</h1>} />
-          <Route path="sign-up" element={<h1>Sign Up Page</h1>} />
+          <Route path="sign-in" element={<UserAuthForm type="sign-in" />} />
+          <Route path="sign-up" element={<UserAuthForm type="sign-up" />} />
         </Route>
         <Route path="*" element={<Error />} />
       </Routes>
---
+++ src/components/input.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InputBox = () => {
+  return (
+    <div>Input Box</div>
+  )
+}
+
+export default InputBox;
