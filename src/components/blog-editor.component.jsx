1795380b7cb1149b266f9da975b41ab20011b1a5
UnicornDad8 <shiningforce972@gmail.com>
2025-05-29 19:52:12 -0300

    Terminar la funcionalidad del blog banner

--- src/components/blog-editor.component.jsx
+++ src/components/blog-editor.component.jsx
@@ -1,7 +1,33 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 import logo from "../imgs/logo.png";
+import AnimationWrapper from "../common/page-animation";
+import banner from "../imgs/banner.png"; 
+import { uploadImage } from "../common/aws";
+import { Toaster, toast } from "react-hot-toast";
 
 const BlogEditor = () => {
+  let blogBannerRef = useRef();
+
+  const handleBannerUpload = (e) => {
+    let img = e.target.files[0]; 
+    
+    if(img) {
+      let loadingToast = toast.loading("Uploading image...");
+
+      uploadImage(img).then((url) => {
+        if(url) {
+          toast.dismiss(loadingToast);
+          toast.success("Image uploaded successfully!");
+          blogBannerRef.current.src = url;
+        }
+      }).catch((err) => {
+        toast.dismiss(loadingToast);
+        return toast.error(err);
+      })
+    }
+  }
+
   return (
     <>
       <nav className="navbar">
@@ -21,6 +47,30 @@ const BlogEditor = () => {
           </button>
         </div>
       </nav>
+      <Toaster />
+
+      <AnimationWrapper>
+        <section>
+          <div className="mx-auto max-w-[900px] w-full">
+            <div className="relative transition-opacity duration-500 bg-white border-4 aspect-video hover:opacity-60 border-grey">
+              <label htmlFor="uploadBanner">
+                <img 
+                  ref={blogBannerRef}
+                  src={banner}
+                  className="z-20"
+                />
+                <input 
+                  id="uploadBanner"
+                  type="file"
+                  accept=".png, .jpg, .jpeg"
+                  hidden
+                  onChange={handleBannerUpload}
+                />
+              </label>
+            </div>
+          </div>
+        </section>
+      </AnimationWrapper>
     </>
   )
 }
