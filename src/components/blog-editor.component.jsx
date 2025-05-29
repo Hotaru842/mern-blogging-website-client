91aaee7a2ecc3563b6d12aaffccd0d152fa6518e
UnicornDad8 <shiningforce972@gmail.com>
2025-05-29 16:25:57 -0300

    Agregar blog editor navbar

--- src/components/blog-editor.component.jsx
+++ src/components/blog-editor.component.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import logo from "../imgs/logo.png";
 
 const BlogEditor = () => {
   return (
-    <div>Blog Editor from component</div>
+    <>
+      <nav className="navbar">
+        <Link to="/" className="flex-none w-10">
+          <img src={logo} alt="logo" />
+        </Link>
+        <p className="w-full text-black max-md:hidden line-clamp-1">
+          New Blog
+        </p>
+
+        <div className="flex gap-4 ml-auto">
+          <button className="btn-dark">
+            Publish
+          </button>
+          <button className="btn-light">
+            Save Draft
+          </button>
+        </div>
+      </nav>
+    </>
   )
 }
 
