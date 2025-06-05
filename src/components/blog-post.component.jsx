895add670b9711cbc6c3a1ce81db4d9c988af897
UnicornDad8 <shiningforce972@gmail.com>
2025-06-05 14:00:56 -0300

    Terminar el componente BlogPostCard

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import { getDay } from '../common/date';
 
 const BlogPostCard = ({ content, author }) => {
@@ -6,6 +7,7 @@ const BlogPostCard = ({ content, author }) => {
   let { fullname, username, profile_img } = author;
  
   return (
+    <Link to={`/blog/${id}`} className="flex items-center gap-8 pb-5 mb-4 border-b border-grey">
     <div className="w-full">
       <div className="flex items-center gap-2 mb-7">
         <img 
@@ -16,7 +18,23 @@ const BlogPostCard = ({ content, author }) => {
         <p className="line-clamp-1">{fullname} @{username}</p>
         <p className="min-w-fit">{getDay(publishedAt)}</p>
       </div>
+      <h1 className="blog-title">{title}</h1>
+      <p className="my-3 text-xl leading-7 font-gelasio max-sm:hidden md:max-[1100px]:hidden line-clamp-2">{desc}</p>
+      <div className="flex gap-4 mt-7">
+        <span className="px-4 py-1 btn-light">{tags[0]}</span>
+        <span className="flex items-center gap-2 ml-3 text-dark-grey">
+          <i className="text-xl fi fi-br-heart" />  
+          {total_likes}
+        </span>
+      </div>
     </div>
+    <div className="h-28 aspect-square bg-grey">
+      <img 
+        src={banner}
+        className="object-cover w-full h-full aspect-square"
+      />
+    </div>
+    </Link>
   )
 }
 
