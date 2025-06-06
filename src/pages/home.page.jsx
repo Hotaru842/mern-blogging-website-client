184db7975271dffddf49cf0485f8153e9e77de92
UnicornDad8 <shiningforce972@gmail.com>
2025-06-06 18:34:19 -0300

    Agregar el aside trending del HomePage

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -9,6 +9,7 @@ import MinimalBlogCard from '../components/nobanner-blog-post.component';
 const HomePage = () => {
   const [blogs, setBlogs] = useState(null);
   const [trendingBlogs, setTrendingBlogs] = useState(null);
+  let categories = ["programming", "cryptocurrency", "hollywood", "film making", "social media", "cooking", "sweet", "tech", "AI", "finances", "travel"];
 
   const fetchLatestBlogs = () => {
     axios.get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
@@ -60,8 +61,33 @@ const HomePage = () => {
             </>
           </InPageNavigation>
         </div>
-        <div>
-
+        <div className="min-w-[40%] lg:min-w-[400px] max-w-min border-l border-grey pl-8 pt-3 max-md:hidden">
+            <div className="flex flex-col gap-10">
+              <div>
+                <h2 className="mb-8 !text-xl font-medium">Stories from all interests</h2>
+                <div className="flex flex-wrap gap-3">
+                  {
+                    categories.map((category, i) => {
+                      return <button className="tag" key={i}>
+                        {category}
+                      </button>
+                    })
+                  }
+                </div>
+              </div>
+              <div>
+                <h2 className="!text-xl font-medium mb-8">
+                  Trending <i className="fi fi-br-arrow-trend-up" />
+                </h2>
+                {
+                  trendingBlogs === null ? <Loader /> : trendingBlogs.map((blog, i) => {
+                    return <AnimationWrapper transition={{ duration: 1, delay: i*.1 }} key={i}>
+                      <MinimalBlogCard blog={blog} index={i} />
+                    </AnimationWrapper>
+                  })
+                }
+              </div>
+        </div>  
         </div>
       </section>
     </AnimationWrapper>
