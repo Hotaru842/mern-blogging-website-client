68fd6d5081843390fb26389755183457f90fd4d2
UnicornDad8 <shiningforce972@gmail.com>
2025-06-12 15:51:52 -0300

    Agregar un fetch blogs en perfil de usuario para ver los blogs creados por dicho usuario

--- src/pages/profile.page.jsx
+++ src/pages/profile.page.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import AnimationWrapper from '../common/page-animation';
 import Loader from '../components/loader.component';
 import AboutUser from '../components/about.component';
+import { filterPaginationData } from '../common/filter-pagination-data';
 
 export const profileDataStructure = {
   personal_info: {
@@ -26,6 +27,7 @@ const ProfilePage = () => {
   const [profile, setProfile] = useState(profileDataStructure);
   const [loading, setLoading] = useState(true);
   const { userAuth: { username }} = useContext(UserContext)
+  const [blogs, setBlogs] = useState(null);
 
   const { personal_info: { fullname, username: profile_username, profile_img, bio },
   account_info: {total_posts, total_reads}, social_links, joinedAt} = profile;
@@ -34,11 +36,34 @@ const ProfilePage = () => {
     axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-profile", { username: profileId })
     .then(({ data: user }) => {
       setProfile(user);
+      getBlogs({ user_id: user._id })
       setLoading(false);
     })
     .catch(err => {
       console.log(err);
       setLoading(false);
+    })
+  }
+
+  const getBlogs = ({ page = 1, user_id }) => {
+    user_id = user_id == undefined ? blogs.user_id : user_id;
+
+    axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/search-blogs", {
+      author: user_id,
+      page
+    })
+    .then(async ({ data }) => {
+      let formatedData = await filterPaginationData({
+        state: blogs,
+        data: data.blogs,
+        page,
+        countRoute: "/search-blogs-count",
+        data_to_send: { author: user_id }
+      })
+
+      formatedData.user_id = user_id;
+
+      setBlogs(formatedData);
     })
   }
 
