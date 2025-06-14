665f77534d4ac15e7cbc418296ae8d8af6deed5e
UnicornDad8 <shiningforce972@gmail.com>
2025-06-14 18:14:47 -0300

    hacer fetch del blog object en la ruta '/get-blog'

--- src/pages/blog.page.jsx
+++ src/pages/blog.page.jsx
@@ -1,7 +1,23 @@
-import { useParams } from 'react-router-dom';
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const BlogPage = () => {
   const { blog_id } = useParams();
+
+  const fetchBlog = () => {
+    axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-blog", { blog_id })
+    .then(({data: { blog }}) => {
+      console.log(blog);
+    })
+    .catch(err => {
+      console.log(err);
+    }) 
+  }
+
+  useEffect(() => {
+    fetchBlog();
+  }, [])
 
   return (
     <div>Blog Page for - {blog_id}</div>
