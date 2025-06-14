3b935dbf64a13a5010ab8ff92264a95249749cc7
UnicornDad8 <shiningforce972@gmail.com>
2025-06-14 18:28:09 -0300

    haciendo un set del state

--- src/pages/blog.page.jsx
+++ src/pages/blog.page.jsx
@@ -1,14 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
+import axios from "axios"; 
+
+export const blogStructure = {
+  title: "",
+  desc: "",
+  content: [],
+  tags: [],
+  author: { personal_info: {}},
+  banner: "",
+  publishedAt: "",
+}
 
 const BlogPage = () => {
   const { blog_id } = useParams();
+  const [blog, setBlog] = useState(blogStructure);
+  const { title, content, banner, author: { personal_info: {
+    fullname, username, profile_img
+  }}, publishedAt } = blog;
 
   const fetchBlog = () => {
     axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-blog", { blog_id })
     .then(({data: { blog }}) => {
-      console.log(blog);
+      setBlog(blog);
     })
     .catch(err => {
       console.log(err);
@@ -20,7 +34,7 @@ const BlogPage = () => {
   }, [])
 
   return (
-    <div>Blog Page for - {blog_id}</div>
+    <div>Blog Page for - {blog?.title}</div>
   )
 }
 
