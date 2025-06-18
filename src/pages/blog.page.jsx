3bc21a5ee48b6c57d500db2b6991ca2c2cf4b662
UnicornDad8 <shiningforce972@gmail.com>
2025-06-18 15:08:48 -0300

    Haciendo fetch de comments con axios

--- src/pages/blog.page.jsx
+++ src/pages/blog.page.jsx
@@ -7,7 +7,7 @@ import { getDay } from "../common/date";
 import BlogInteraction from "../components/blog-interaction.component";
 import BlogPostCard from "../components/blog-post.component";
 import BlogContent from "../components/blog-content.component";
-import CommentsContainer from "../components/comments.component";
+import CommentsContainer, { fetchComments } from "../components/comments.component";
 
 export const blogStructure = {
   title: "",
@@ -35,7 +35,14 @@ const BlogPage = () => {
 
   const fetchBlog = () => {
     axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-blog", { blog_id })
-    .then(({data: { blog }}) => {
+    .then(async ({data: { blog }}) => {
+
+      blog.comments = await fetchComments({ blog_id: blog._id,
+      setParentCommentCountFun: setTotalParentCommentsLoaded
+      });
+
+      setBlog(blog);
+
       axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/search-blogs", {
         tag: blog.tags,
         limit: 6,
@@ -45,7 +52,6 @@ const BlogPage = () => {
         setSimilarBlogs(data.blogs);
       })
 
-      setBlog(blog);
       setLoading(false);
     })
     .catch(err => {
