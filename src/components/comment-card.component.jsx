01874f4002398aaa305b248a9de362515d359626
UnicornDad8 <shiningforce972@gmail.com>
2025-06-20 15:51:07 -0300

    Empezar funcionalidad de responder comentarios

--- src/components/comment-card.component.jsx
+++ src/components/comment-card.component.jsx
@@ -1,8 +1,24 @@
+import { useContext, useState } from 'react';
+import { UserContext } from '../App';
 import { getDay } from '../common/date';
+import { toast } from 'react-hot-toast';
+import CommentField from './comment-field.component';
 
 const CommentCard = ({ index, leftVal, commentData }) => {
   let { commented_by: { personal_info: { profile_img, fullname, username }},
-  commentedAt, comment } = commentData;
+  commentedAt, comment, _id } = commentData;
+
+  let { userAuth: {access_token}} = useContext(UserContext);
+
+  const [isReplying, setIsReplying] = useState(false);
+
+  const handleReplyClick = () => {
+    if(!access_token) {
+      return toast.error("You must login first to leave a reply")
+    }
+
+    setIsReplying(prevVal => !prevVal);
+  }
 
   return (
     <div className="w-full" style={{ paddingLeft: `${leftVal * 10}px`}}>
@@ -18,6 +34,25 @@ const CommentCard = ({ index, leftVal, commentData }) => {
         </div>
         
         <p className="font-gelasio !text-xl ml-3">{comment}</p>
+
+        <div className="flex items-center gap-5 mt-5">
+          <button 
+            className="underline"
+            onClick={handleReplyClick}
+          >Reply</button>
+        </div>
+        {
+          isReplying ? 
+          <div className="mt-8">
+            <CommentField 
+              action="reply" 
+              index={index} 
+              replyingTo={_id} 
+              setIsReplying={setIsReplying}
+            />
+          </div> 
+          : null
+        }
       </div>
     </div>
   )
--- src/components/comment-field.component.jsx
+++ src/components/comment-field.component.jsx
@@ -4,7 +4,8 @@ import { BlogContext } from '../pages/blog.page';
 import { Toaster, toast } from 'react-hot-toast';
 import axios from 'axios';
 
-const CommentField = ({ action }) => {
+const CommentField = ({ action, index = undefined, 
+  replyingTo = undefined, setIsReplying }) => {
   let { blog, setBlog, setTotalParentCommentsLoaded, blog: { _id, author: { _id: blog_author }, 
   comments, comments: { results: commentsArr }, activity, activity: { total_comments, total_parent_comments } }} = useContext(BlogContext);
   let { userAuth: { access_token, username, fullname, profile_img }} = useContext(UserContext);
@@ -20,7 +21,7 @@ const CommentField = ({ action }) => {
     }
 
     axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/add-comment", {
-      _id, blog_author, comment
+      _id, blog_author, comment, replying_to: replyingTo
     }, {
       headers: {
         "Authorization": `Bearer ${access_token}`
@@ -32,11 +33,25 @@ const CommentField = ({ action }) => {
       data.commented_by = { personal_info: { username, profile_img, fullname }}
       let newCommentArr;
 
-      data.childrenLevel = 0;
+      if(replyingTo) {
+        commentsArr[index].children.push(data._id);
 
-      newCommentArr = [ data, ...commentsArr ] 
+        data.childrenLevel = commentsArr[index].childrenLevel + 1;
+        data.parentIndex = index;
 
-      let parentCommentIncrementVal = 1;
+        commentsArr[index].isReplyLoaded = true;
+
+        commentsArr.splice(index + 1, 0, data);
+
+        newCommentArr = commentsArr;
+        setIsReplying(false);
+      } else {
+        data.childrenLevel = 0;
+
+        newCommentArr = [ data, ...commentsArr ] 
+      }
+
+      let parentCommentIncrementVal = replyingTo ? 0 : 1;
 
       setBlog({ ...blog, comments: { ...comments, results: newCommentArr },
       activity: { ...activity, total_comments: total_comments + 1,
