3cd8eb9155706070af29a3b7b5918ea7b4fb4cd9
UnicornDad8 <shiningforce972@gmail.com>
2025-06-27 18:54:16 -0300

    Agregar los botones de filtro en notifications page

--- src/pages/notifications.page.jsx
+++ src/pages/notifications.page.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { useState } from "react";
 
 const Notifications = () => {
+  const [filter, setFilter] = useState("all");
+
+  let filters = ["all", "like", "comment", "reply"];
+
   return (
-    <div>Notifications Page</div>
+    <div>
+      <h1 className="max-md:hidden">Recent Notifications</h1>
+      <div className="flex gap-6 my-8">
+        {
+          filters.map((filterName, i) => {
+            return <button key={i} className={"py-2 " + (filter == filterName ? "btn-dark" : " btn-light")}>{filterName}</button>
+          })
+        }
+      </div>
+    </div>
   )
 }
 
