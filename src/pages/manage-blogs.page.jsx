8eefd705cd527b88badabe4ff626ffc893aaafd3
UnicornDad8 <shiningforce972@gmail.com>
2025-07-04 14:31:58 -0300

    Poner el input de buscar query y hacerlo funcionar

--- src/pages/manage-blogs.page.jsx
+++ src/pages/manage-blogs.page.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../App";
 import axios from "axios";
 import { filterPaginationData } from "../common/filter-pagination-data";
+import { Toaster } from "react-hot-toast";
 
 const ManageBlogs = () => {
   const [blogs, setBlogs] = useState(null);
@@ -49,11 +50,41 @@ const ManageBlogs = () => {
         getBlogs({ page: 1, draft: true });
       }
     }
-  }, [access_token, blogs, drafts, query])
+  }, [access_token, blogs, drafts, query]);
+
+  const handleSearch = (e) => {
+    let searchQuery = e.target.value; 
+
+    setQuery(searchQuery);
+
+    if(e.keyCode == 13 && searchQuery.length > 0) {
+      setBlogs(null);
+      setDrafts(null);
+    }
+  }
+
+  const handleChange = (e) => {
+    if(!e.target.value.length) {
+      setQuery("");
+      setBlogs(null);
+      setDrafts(null);
+    }
+  }
 
   return (
     <>
-      <h1>Manage Bolgs</h1>
+      <h1 className="max-md:hidden">Manage Bolgs</h1>
+      <Toaster />
+      <div className="relative mb-10 max-md:mt-5 md:mt-8">
+        <input 
+          type="search"
+          className="w-full p-4 pl-12 pr-6 rounded-full bg-grey placeholder:text-dark-grey"
+          placeholder="Search Blogs"
+          onChange={handleChange}
+          onKeyDown={handleSearch}
+        />
+        <i className="fi fi-br-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 !text-xl text-dark-grey"/>
+      </div>
     </>
   )
 }
